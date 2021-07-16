cfb07c1778ef0a43d400831c2f104053a7ba4f1c
Jacob Sutherland <61425692+JakLoveMD@users.noreply.github.com>
2021-07-15 18:22:01 -0600

    Store
    added store page and updated library

--- js/calander.js
+++ js/calander.js
@@ -1,34 +1,10 @@
+
+import Calendar from 'mdb-calendar';
 const events = [
   {
-    summary: 'JS Conference',
-    start: {
-      date: Calendar.moment.format('DD/MM/YYYY'),
-    },
-    end: {
-      date: Calendar.moment.format('DD/MM/YYYY'),
-    },
-    color: {
-      background: '#1266F1',
-      foreground: 'white',
-    },
-  },
-  {
-    summary: 'Vue Meetup',
-    start: {
-      date: Calendar.moment.add(1, 'day').format('DD/MM/YYYY'),
-    },
-    end: {
-      date: Calendar.moment.add(5, 'day').format('DD/MM/YYYY'),
-    },
-    color: {
-      background: '#00B74A',
-      foreground: 'white',
-    },
-  },
-  {
-    summary: 'Angular Meetup',
+    summary: 'Work on Site',
     description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+      'Get this Calendar to work, idk why it isnt really',
     start: {
       date: Calendar.moment.subtract(3, 'day').format('DD/MM/YYYY'),
       dateTime: Calendar.moment.subtract(3, 'day').format('DD/MM/YYYY') + ' 10:00',
@@ -42,50 +18,8 @@ const events = [
       foreground: 'white',
     },
   },
-  {
-    summary: 'React Meetup',
-    start: {
-      date: Calendar.moment.add(5, 'day').format('DD/MM/YYYY'),
-    },
-    end: {
-      date: Calendar.moment.add(8, 'day').format('DD/MM/YYYY'),
-    },
-    color: {
-      background: '#39C0ED',
-      foreground: 'white',
-    },
-  },
-  {
-    summary: 'Meeting',
-    start: {
-      date: Calendar.moment.add(1, 'day').format('DD/MM/YYYY'),
-      dateTime: Calendar.moment.add(1, 'day').format('DD/MM/YYYY') + ' 8:00',
-    },
-    end: {
-      date: Calendar.moment.add(1, 'day').format('DD/MM/YYYY'),
-      dateTime: Calendar.moment.add(1, 'day').format('DD/MM/YYYY') + ' 12:00',
-    },
-    color: {
-      background: '#1266F1',
-      foreground: 'white',
-    },
-  },
-  {
-    summary: 'Call',
-    start: {
-      date: Calendar.moment.add(2, 'day').format('DD/MM/YYYY'),
-      dateTime: Calendar.moment.add(2, 'day').format('DD/MM/YYYY') + ' 11:00',
-    },
-    end: {
-      date: Calendar.moment.add(2, 'day').format('DD/MM/YYYY'),
-      dateTime: Calendar.moment.add(2, 'day').format('DD/MM/YYYY') + ' 14:00',
-    },
-    color: {
-      background: '#FFA900',
-    },
-  }
 ];
 
-const calendarElement = document.getElementById('calendar');
+const calendarElement = document.getElementById('calendar-7');
 const calendarInstance = Calendar.getInstance(calendarElement);
-calendarInstance.addEvents(events);// JavaScript Document
+calendarInstance.addEvents(events);
